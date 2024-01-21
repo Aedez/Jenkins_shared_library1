bcbee116940072370654573cfb47a4d10c0f7aa6
Michael N.K <55757253+Aedez@users.noreply.github.com>
2024-01-21 01:05:13 +0000

    Update dockerBuild.groovy
    removed key

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=675fc6c560msha629ddf48127686p1e8038jsnfe813a9f8f3b -t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
